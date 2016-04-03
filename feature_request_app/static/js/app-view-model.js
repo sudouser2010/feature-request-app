83220e8f5cfa395ae444683fa6d73941521ded44
herbert <herbertdawkins@gmail.com>
2016-04-03 10:43:40 -0400

    modified template and view model

--- feature_request_app/static/js/app-view-model.js
+++ feature_request_app/static/js/app-view-model.js
@@ -1,5 +1,5 @@
 
-function Client(name, id, disable) {
+function OptionItem(name, id, disable) {
     var self = this;
 
     self.id = ko.observable(id);
@@ -17,13 +17,20 @@ function AppViewModel() {
     self.url=ko.observable("");
 
     self.clients = [
-        new Client( 'Select Client', 0, true),
-        new Client( 'A', 1, false),
-        new Client( 'B', 2, false),
-        new Client( 'C', 3, false),
+        new OptionItem( 'Select Client', 0, true),
+        new OptionItem( 'A', 1, false),
+        new OptionItem( 'B', 2, false),
+        new OptionItem( 'C', 3, false),
+    ];
+    self.productArea = [
+        new OptionItem( 'Select Product Area', 0, true),
+        new OptionItem( 'Policies', 1, false),
+        new OptionItem( 'Billing', 2, false),
+        new OptionItem( 'Claims', 3, false),
+        new OptionItem( 'Reports', 4, false),
     ];
     self.setOptionAsDisabled = function(option, item) {
-        ko.applyBindingsToNode(option, {disable: item.disable}, item);
+        ko.applyBindingsToNode(option, {disable: item.disabled}, item);
     };
 };
 
