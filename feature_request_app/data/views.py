419e0749e6e97bc8f395e7a947a25f2807b55ffe
herbert <herbertdawkins@gmail.com>
2016-04-03 17:19:23 -0400

    overridded create method of ModelViewSet so some feature requests are reordered right before creating the new feature request

--- feature_request_app/data/views.py
+++ feature_request_app/data/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django.db.models.expressions import F
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from create.models import FeatureRequest
@@ -9,7 +10,7 @@ class FeatureRequestViewset(viewsets.ModelViewSet):
     queryset = FeatureRequest.objects.all()
     serializer_class = FeatureRequestSerializer
 
-    def list(self, request):
+    def list(self, request, *args, **kwargs):
         data = dict()
         for client in FeatureRequest.ClientChoices:
             client_data = FeatureRequest.objects.filter(
@@ -17,3 +18,26 @@ class FeatureRequestViewset(viewsets.ModelViewSet):
             ).values()
             data[client[1]] = client_data
         return Response(data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.reorder_feature_requests(serializer)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def reorder_feature_requests(self, serializer):
+        """
+            This gets all the feature requests with an equal or lower priority for a given client
+            and reduces the priority of those feature requests by one.
+
+            This makes space so the new feature request can be inserted at priority.
+        """
+        client = serializer.validated_data['client']
+        priority = serializer.validated_data['priority']
+        affected_feature_requests = FeatureRequest.objects.filter(
+            client=client,
+            priority__gte=priority
+        )
+        affected_feature_requests.update(priority=F('priority')+1)
