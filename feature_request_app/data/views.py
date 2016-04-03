91970200ac0510902a754721e3f62af11fecd13e
herbert <herbertdawkins@gmail.com>
2016-04-03 00:30:25 -0400

    created class-based view as the api endpoint for api/feature-request

--- feature_request_app/data/views.py
+++ feature_request_app/data/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from create.models import FeatureRequest
+from .serializers import FeatureRequestSerializer
+
+
+class FeatureRequestViewset(viewsets.ModelViewSet):
+    queryset = FeatureRequest.objects.all()
+    serializer_class = FeatureRequestSerializer
