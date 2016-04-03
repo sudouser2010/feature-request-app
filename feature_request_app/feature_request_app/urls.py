2425169d0bd3e1b722c8d16a2707089be2866520
herbert <herbertdawkins@gmail.com>
2016-04-03 00:31:05 -0400

    modified urls so the api works

--- feature_request_app/feature_request_app/urls.py
+++ feature_request_app/feature_request_app/urls.py
@@ -1,8 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
 
 from create.views import single_page_app
+from data.views import FeatureRequestViewset
 
+
+router = routers.DefaultRouter()
+router.register(r'feature-requests', FeatureRequestViewset)
 
 urlpatterns = [
+
+    url(r'^api/', include(router.urls)),
     url(r'^',   single_page_app),
 ]
