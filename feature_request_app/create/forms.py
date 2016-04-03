423c825760478dd05b4b39358f4946ee26a18328
herbert <herbertdawkins@gmail.com>
2016-04-02 22:14:01 -0400

    implemented form with the convential Django approach

--- feature_request_app/create/forms.py
+++ feature_request_app/create/forms.py
@@ -19,6 +19,7 @@ class FeatureRequestForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Enter Title'}),
             'description': forms.TextInput(attrs={'placeholder': 'Enter Description'}),
-            'target_date': forms.TextInput(attrs={'placeholder': 'Enter Date'}),
-            'ticket_url': forms.TextInput(attrs={'placeholder': 'Enter URL'}),
+            'target_date': forms.TextInput(attrs={'placeholder': 'Enter Target Date'}),
+            'priority': forms.NumberInput(attrs={'min': 1, 'placeholder': 'Enter Priority'}),
+            'ticket_url': forms.TextInput(attrs={'placeholder': 'Enter Ticket URL'}),
             }
