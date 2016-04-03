daaeb62f0f2450667371a7b793de65f340ce3c06
herbert <herbertdawkins@gmail.com>
2016-04-03 01:47:18 -0400

    api now returns data on clients arranged by client type

--- feature_request_app/data/views.py
+++ feature_request_app/data/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from create.models import FeatureRequest
 from .serializers import FeatureRequestSerializer
@@ -7,3 +8,12 @@ from .serializers import FeatureRequestSerializer
 class FeatureRequestViewset(viewsets.ModelViewSet):
     queryset = FeatureRequest.objects.all()
     serializer_class = FeatureRequestSerializer
+
+    def list(self, request):
+        data = dict()
+        for client in FeatureRequest.ClientChoices:
+            client_data = FeatureRequest.objects.filter(
+                client=client[0]
+            ).values()
+            data[client[1]] = client_data
+        return Response(data)
