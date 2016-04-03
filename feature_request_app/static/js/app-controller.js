60d9e6ac372a411a65ddb4157b3312eb7b75f8ec
herbert <herbertdawkins@gmail.com>
2016-04-02 22:10:42 -0400

    changed default message on product area input

--- feature_request_app/static/js/app-controller.js
+++ feature_request_app/static/js/app-controller.js
@@ -1,3 +1,3 @@
 
 $("#id_client").prepend("<option value='' selected disabled>Choose Client</option>");
-$("#id_product_area").prepend("<option value='' selected disabled>Choose Client</option>");
+$("#id_product_area").prepend("<option value='' selected disabled>Choose Product Area</option>");
