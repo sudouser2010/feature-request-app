7e41f262e417a0d8f173548c6e439cf78f3cd95c
herbert <herbertdawkins@gmail.com>
2016-04-03 10:21:38 -0400

    modified app's view model so form elements have binding

--- feature_request_app/static/js/app-controller.js
+++ feature_request_app/static/js/app-controller.js
@@ -1,3 +1,31 @@
 
-$("#id_client").prepend("<option value='' selected disabled>Choose Client</option>");
-$("#id_product_area").prepend("<option value='' selected disabled>Choose Product Area</option>");
+function Client(name, id, disable) {
+    var self = this;
+
+    self.id = ko.observable(id);
+    self.name = ko.observable(name);
+    self.disabled = ko.observable(disable);
+};
+
+function AppViewModel() {
+    var self = this;
+
+    self.title=ko.observable("");
+    self.description=ko.observable("");
+    self.priority=ko.observable("");
+    self.date=ko.observable("");
+    self.url=ko.observable("");
+
+    self.clients = [
+        new Client( 'Select Client', 0, true),
+        new Client( 'A', 1, false),
+        new Client( 'B', 2, false),
+        new Client( 'C', 3, false),
+    ];
+    self.setOptionAsDisabled = function(option, item) {
+        ko.applyBindingsToNode(option, {disable: item.disable}, item);
+    };
+};
+
+var vm = new AppViewModel();
+ko.applyBindings(vm);
