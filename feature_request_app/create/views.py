2c58dd036de276e2a41bec86896bd9bcd5b90cd6
herbert <herbertdawkins@gmail.com>
2016-04-03 23:07:12 -0400

    added server-side validation that ensures the priority cannot be more than the number of clients of a certain type

--- feature_request_app/create/views.py
+++ feature_request_app/create/views.py
@@ -5,8 +5,4 @@ from .forms import FeatureRequestForm
 
 def single_page_app(request):
     form = FeatureRequestForm(request.POST)
-
-    if form.is_valid():
-        pass
-
     return render(request, 'single-page-app.html', {'form': form})
--- feature_request_app/data/serializers.py
+++ feature_request_app/data/serializers.py
@@ -16,3 +16,15 @@ class FeatureRequestSerializer(serializers.ModelSerializer):
             'ticket_url',
             'product_area'
         )
+
+    def validate(self, data):
+        if data['priority'] and data['client']:
+            number_of_clients_for_type = FeatureRequest.objects.filter(
+                client=data['client']
+            ).count()
+            if data['priority'] > number_of_clients_for_type:
+                client = FeatureRequest.ClientChoices[data['client']][1]
+                message = 'Priority can not exceed number of client {}'.format(client)
+                raise serializers.ValidationError(message)
+
+        return data
